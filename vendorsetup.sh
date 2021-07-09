f17da6dfa864426c2119cbfae90a690d4fad3ac1
tushar <tusharredekar007@gmail.com>
2021-07-09 13:31:59 +0530

    sm6250-common: add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+git clone --depth=1 --single-branch https://github.com/LineageOS/android_vendor_qcom_opensource_healthd-ext vendor/qcom/opensource/healthd-ext 
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi 
+git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b master vendor/xiaomi/miuicamera 
